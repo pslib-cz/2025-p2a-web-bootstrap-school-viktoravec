c8bc5f2e629f21d92fb6730c7a17263ad1c785e7
viktoravec <viktor.oravec.024@pslib.cz>
2025-09-12 13:18:00 +0200

    WEB - update z hodiny

--- src/main.js
+++ src/main.js
@@ -1,9 +1,6 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import './style.css'
 
 document.querySelector('#app').innerHTML = `
   <div>
